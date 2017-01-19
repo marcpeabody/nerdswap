c68293ca7f2c460ca468d8706715a335282ed673
Marc Peabody <marcpeabody@yahoo.com>
2017-01-18 21:20:55 -0500

    spell out the card object in the UI for now in MTGCard

--- src/components/MTGCard.js
+++ src/components/MTGCard.js
@@ -12,7 +12,36 @@ class MTGCard extends Component {
   }
   render() {
     const {card} = this.state;
-    return card ? (<img alt={card.name} src={card.imageUrl} role="presentation"/>) : <div>Loading</div>
+    if (card) {
+      const legalities = card.legalities.map((leg, i) => <div key={i}>format: {leg.format} - legality: {leg.legality}</div>)
+      return (
+        <div>
+          <ul>
+            <li>artist: {card.artist}</li>
+            <li>cmc: {card.cmc}</li>
+            <li>id: {card.id}</li>
+            <li>imageUrl: {card.imageUrl}</li>
+            <li>layout: {card.layout}</li>
+            <li>legalities: {legalities}</li>
+            <li>manaCost: {card.manaCost}</li>
+            <li>multiverseid: {card.multiverseid}</li>
+            <li>name: {card.name}</li>
+            <li>originalText: {card.originalText}</li>
+            <li>originalType: {card.originalType}</li>
+            <li>printings: {card.printings.join(',')}</li>
+            <li>rarity: {card.rarity}</li>
+            <li>set: {card.set}</li>
+            <li>setName: {card.setName}</li>
+            <li>text: {card.text}</li>
+            <li>type: {card.type}</li>
+            <li>types: {card.types}</li>
+          </ul>
+          <img alt={card.name} src={card.imageUrl} role="presentation"/>
+        </div>
+      );
+    } else {
+      return (<div>Loading</div>);
+    }
   }
 }
 
